6788efedf7cf6385a00b71f84632473104137ac7
Loooonnny <sarahguillemard@outlook.com>
2021-04-30 14:32:00 +0200

    Update CSS

--- src/index.js
+++ src/index.js
@@ -107,7 +107,7 @@ function showWeather(response) {
   let windElement = document.querySelector("#wind-speed");
   windElement.innerHTML = `${wind}km/h`;
 
-  let iconElement = document.querySelector("#weather-icon");
+  let iconElement = document.querySelector("#current-weather-icon");
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
